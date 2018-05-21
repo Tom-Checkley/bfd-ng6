ebf887496e686d28489094a90be3be99b74dc276
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-05-21 17:02:33 +0100

    testimonial class and getter service

---
+++ src/app/_classes/testimonial.ts
@@ -0,0 +1,10 @@
+export class Testimonial {
+  public name: string;
+  public stars: number;
+  public outOf: number;
+  public date: number;
+  public location?: string;
+  public body?: string;
+  public id?: string;
+  public verified?: boolean;
+}
