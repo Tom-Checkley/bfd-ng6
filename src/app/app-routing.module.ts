55da4ca8615fa0522de82490da600d926f5aa606
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-06-04 22:21:35 +0100

    service routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,21 @@ import { ContactComponent } from './_components/public/contact/contact.component
 import { TestimonialsComponent } from './_components/public/testimonials/testimonials/testimonials.component';
 import { VerifyTestimonialsComponent } from './_components/admin/verify-testimonials/verify-testimonials.component';
 import { ServicesComponent } from './_components/public/services/services/services.component';
+import { CctvComponent } from './_components/public/services/cctv/cctv.component';
+import { ClearanceComponent } from './_components/public/services/clearance/clearance.component';
+import { InstallationComponent } from './_components/public/services/installation/installation.component';
+import { RepairsComponent } from './_components/public/services/repairs/repairs.component';
+import { TankerComponent } from './_components/public/services/tanker/tanker.component';
 
 const routes: Routes = [
   { path: 'admin/verify-testimonials', component: VerifyTestimonialsComponent },
   { path: 'admin/company-info', component: CompanyInfoComponent },
   { path: 'admin', component: AdminHomeComponent },
+  { path: 'services/cctv', component: CctvComponent },
+  { path: 'services/clearance', component: ClearanceComponent },
+  { path: 'services/installation', component: InstallationComponent },
+  { path: 'services/repairs', component: RepairsComponent },
+  { path: 'services/tanker', component: TankerComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'testimonials', component: TestimonialsComponent },
   { path: 'contact', component: ContactComponent },
