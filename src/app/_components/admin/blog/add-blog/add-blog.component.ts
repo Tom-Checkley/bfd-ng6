b5d80f25f443e97e0f709ef3b613bd05af2bfbc5
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-06-09 09:01:45 +0100

    upload image functionality - needs adding to service

--- src/app/_components/admin/blog/add-blog/add-blog.component.ts
+++ src/app/_components/admin/blog/add-blog/add-blog.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Blog } from '../../../../_classes/blog';
+import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-blog',
@@ -9,17 +12,35 @@ import { Blog } from '../../../../_classes/blog';
 export class AddBlogComponent implements OnInit {
 
   imageTitle: string;
-  imageSrc: string;
   postTitle: string;
   postLocation: string;
   postBody: string;
   date: Date;
   post: Blog;
   id: any;
+  imageSrc: Observable<string>;
+  uploadPercent: Observable<number>;
+  task: AngularFireUploadTask;
 
-  constructor() { }
+  constructor(private storage: AngularFireStorage) { }
 
   ngOnInit() {
+  }
+
+  uploadImage($event) {
+    const file = $event.target.files[0];
+    const filePath = `blog-images/${Date.now()}_${file.name}`;
+    const fileRef = this.storage.ref(filePath);
+    // console.log(file, fileRef);
+    this.task = this.storage.upload(filePath, file);
+    this.uploadPercent = this.task.percentageChanges();
+    console.log(this.uploadPercent);
+    this.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.imageSrc = fileRef.getDownloadURL();
+        console.log(this.imageSrc);
+      })
+    ).subscribe();
   }
 
   createPost() {}
