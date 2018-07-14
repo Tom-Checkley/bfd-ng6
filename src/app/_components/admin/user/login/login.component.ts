6b69ec3b53cebd4fff3841b0f576522c901ecbfe
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-07-14 14:05:02 +0100

    Login component

--- src/app/_components/admin/user/login/login.component.ts
+++ src/app/_components/admin/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../_services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.userService.login(this.email, this.password)
   }
 
 }
